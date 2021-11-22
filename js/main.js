06c34633480d4face2915d55aad59d6e351aa5d8
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-22 16:19:25 +0100

    creazione ciclo per la stampa di 10 mail

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 var Email = new Vue ({
     el: "#app",
     data: {
-        EmailUno: "",
+        Emails: [
+
+        ],
     },
     mounted() {
         const self = this;
 
         // Ajax asincrono
+        for (i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(result) {
+                self.Emails.push(result.data.response);
+            });
+        }
+        
 
-        axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(function(result) {
-            self.EmailUno = result.data.response;
-        });
     }
 })
